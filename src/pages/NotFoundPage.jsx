37792fdb376e0258662fdc0b79ab20727a6d514e
kerem <alpkerem72@gmail.com>
2026-03-19 17:50:48 +0300

    dosya yolu sorunu düzeltildi.

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SEO from '../components/SEO/SEO';
+import SEO from '../components/SEO';
 
 const NotFoundPage = () => {
   return (
